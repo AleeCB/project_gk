546acff78f114ddbc969f952bce296b13e4c6cf8
AleeCB <alexcb1028@gmail.com>
2023-12-04 21:34:11 -0600

    Se cierra la sesion, edita contraseña

--- app/src/main/java/com/example/gr/UsuarioFragment.java
+++ app/src/main/java/com/example/gr/UsuarioFragment.java
@@ -1,12 +1,29 @@
 package com.example.gr;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,8 +36,12 @@ public class UsuarioFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    private PopupWindow popupWindow;
 
-    // TODO: Rename and change types of parameters
+    private TextView correoTextView;
+    private Button editarContrasenaButton;
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +49,7 @@ public class UsuarioFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsuarioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static UsuarioFragment newInstance(String param1, String param2) {
         UsuarioFragment fragment = new UsuarioFragment();
         Bundle args = new Bundle();
@@ -59,6 +72,119 @@ public class UsuarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false);
+        View view = inflater.inflate(R.layout.fragment_usuario, container, false);
+
+        // Inicializar FirebaseAuth
+        auth = FirebaseAuth.getInstance();
+
+        // Obtener referencia a los elementos de la interfaz de usuario en tu diseño de fragment
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView correoTextView = view.findViewById(R.id.correoTextView);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button editarContrasenaButton = view.findViewById(R.id.editarContrasenaButton);
+        // Obtener referencia al botón de cerrar sesión en el diseño de fragment
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button cerrarSesionButton = view.findViewById(R.id.cerrarSesionButton);
+
+        // Obtener el usuario actualmente autenticado
+        currentUser = auth.getCurrentUser();
+
+        // Verificar si el usuario está autenticado
+        if (currentUser != null) {
+            // Mostrar el correo electrónico en el TextView
+            correoTextView.setText("Correo electrónico: " + currentUser.getEmail());
+        }
+
+        // Configurar el onClickListener para el botón de editar contraseña
+        editarContrasenaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solicitarContrasenaParaMostrarVentanaEmergente();
+            }
+        });
+        // Configurar el onClickListener para el botón de cerrar sesión
+        cerrarSesionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
+
+        return view;
+    }
+
+    private void solicitarContrasenaParaMostrarVentanaEmergente() {
+        // Inflar el diseño personalizado para la ventana emergente
+        View popupView = getLayoutInflater().inflate(R.layout.editar_contra_popup, null);
+
+        // Crear la ventana emergente y configurarla
+        popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        // Configurar referencias a elementos en el diseño de la ventana emergente
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextInputLayout contrasenaActualInputLayout = popupView.findViewById(R.id.contrasenaActualInputLayout);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText contrasenaActualEditText = popupView.findViewById(R.id.contrasenaActualEditText);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextInputLayout nuevaContrasenaInputLayout = popupView.findViewById(R.id.nuevaContrasenaInputLayout);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText nuevaContrasenaEditText = popupView.findViewById(R.id.nuevaContrasenaEditText);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button guardarButton = popupView.findViewById(R.id.guardarButton);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button cancelarButton = popupView.findViewById(R.id.cancelarButton);
+
+        // Configurar el onClickListener para el botón de guardar en la ventana emergente
+        guardarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contrasenaIngresada = contrasenaActualEditText.getText().toString();
+                // Verificar la contraseña ingresada antes de mostrar la ventana emergente
+                verificarContrasenaActualYMostrarVentanaEmergente(contrasenaIngresada, nuevaContrasenaEditText.getText().toString());
+            }
+        });
+
+        // Configurar el onClickListener para el botón de cancelar en la ventana emergente
+        cancelarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cerrar la ventana emergente sin guardar cambios
+                popupWindow.dismiss();
+            }
+        });
+
+        // Mostrar la ventana emergente en el centro de la pantalla
+        popupWindow.showAtLocation(getView(), Gravity.CENTER, 0, 0);
+    }
+
+    private void verificarContrasenaActualYMostrarVentanaEmergente(String contrasenaIngresada, String nuevaContrasena) {
+        // Crear credencial con el correo electrónico actual y la contraseña ingresada
+        AuthCredential credential = EmailAuthProvider.getCredential(currentUser.getEmail(), contrasenaIngresada);
+
+        // Reautenticar al usuario con la contraseña ingresada
+        currentUser.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Reautenticación exitosa, mostrar la ventana emergente
+                            cambiarContrasena(nuevaContrasena);
+                            popupWindow.dismiss();
+                        } else {
+                            // Reautenticación fallida, mostrar un mensaje al usuario
+                            Toast.makeText(getActivity(), "Contraseña incorrecta. Inténtelo de nuevo.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void cambiarContrasena(String nuevaContrasena) {
+        // Cambiar la contraseña del usuario actual
+        currentUser.updatePassword(nuevaContrasena)
+                .addOnCompleteListener(getActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "Contraseña actualizada exitosamente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Error al actualizar la contraseña: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    // Método para cerrar sesión
+    private void cerrarSesion() {
+        FirebaseAuth.getInstance().signOut();
+        // Puedes redirigir al usuario a la pantalla de inicio de sesión o a donde desees
+        startActivity(new Intent(getActivity(), InicioActivity.class));
+        getActivity().finish();
     }
 }
